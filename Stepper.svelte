9d5c519d25f07beb8e25e23874bb1e848ecab669
Jason Kao <jason.kao@columbia.edu>
2023-12-19 13:54:58 -0500

    Update Stepper.svelte

--- Stepper.svelte
+++ Stepper.svelte
@@ -1,30 +1,42 @@
-<script lang="ts">
+<script>
 	import { onDestroy, onMount } from 'svelte';
 
-	export let query = 'section';
+	export let selector = 'section';
 	export let attribute = 'data-index';
 	export let offset = 0.5;
-	export let onStepEnter = (i: number, direction: string) => {};
-	export let onStepExit = (i: number, direction: string) => {};
 	export let debug = false;
 
-	let foreground: HTMLDivElement;
-	let observer: IntersectionObserver;
+	/**
+	 * @param {{index: any, direction: "up" | "down"}} obj An object containing the index of the entered step and the direction of the scroll
+	 */
+	export let onStepEnter = (obj) => {};
+	/**
+	 * @param {{index: any, direction: "up" | "down"}} obj An object containing the index of the exited step and the direction of the scroll
+	 */
+	export let onStepExit = (obj) => {};
+
+	/** @type {HTMLDivElement} */
+	let foreground;
+	/** @type {IntersectionObserver} */
+	let observer;
 
 	let mounted = false;
 	onMount(() => (mounted = true));
-
 	onDestroy(destroyObserver);
 
-	$: if (mounted) makeObserver(`-${offset * 100}% 0px -${100 - offset * 100}% 0px`, query);
+	$: if (mounted) makeObserver(`-${offset * 100}% 0px -${100 - offset * 100}% 0px`, selector);
 
-	function makeObserver(rootMargin: string, query: string) {
+	/**
+	 * @param {string} rootMargin The root margin calculated from the offset
+	 * @param {string} selector The selector used to find the steps
+	 */
+	function makeObserver(rootMargin, selector) {
 		destroyObserver();
 
 		observer = new IntersectionObserver(
 			(entries) => {
 				entries.forEach((entry) => {
-					const index = +(entry.target.getAttribute(attribute) || '');
+					const index = entry.target.getAttribute(attribute);
 					const direction = entry.isIntersecting
 						? entry.boundingClientRect.y < 0
 							? 'up'
@@ -32,15 +44,15 @@
 						: entry.boundingClientRect.y > 0
 						? 'up'
 						: 'down';
-					if (debug && !entry.isIntersecting) console.log(index, entry.isIntersecting, direction);
-					if (entry.isIntersecting) onStepEnter(index, direction);
-					else onStepExit(index, direction);
+					if (debug) console.log(index, entry.isIntersecting, direction);
+					if (entry.isIntersecting) onStepEnter({ index, direction });
+					else onStepExit({ index, direction });
 				});
 			},
 			{ rootMargin }
 		);
 
-		foreground.querySelectorAll(query).forEach((element) => observer.observe(element));
+		foreground.querySelectorAll(selector).forEach((element) => observer.observe(element));
 	}
 
 	function destroyObserver() {
